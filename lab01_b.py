8ea6d8172abd5bbb0af66566998c0379d4d0e46a
Harabali <harangi.balazs@gmail.com>
2018-02-07 01:09:02 +0100

    Create a new task list with solutions.

--- lab01_b.py
+++ lab01_b.py
@@ -7,5 +7,27 @@ def uniqueSortedWords(list):
     return newList
 
 
+def first_n(str,n):
+    if len(str) <= n:
+        return str
+    else:
+        res = str[:n]
+        return res
+
+
+# def main():
 ex1List = ['red', 'white', 'black', 'red', 'green', 'black']
 print(uniqueSortedWords(ex1List))
+
+print(first_n(input('Give a word:'),int(input('Give n:'))))
+
+sum = 0
+for i in range(0,5):
+    n = int(input('Give {}. number: '.format(i)))
+    sum+=n
+print('The sum: ',sum)
+
+# main()
+
+# if __name__ == "__main__":
+#     main()
