735c8c805c189093c9e0fdcd8a862a0ef83ff5ab
Harabali <harangi.balazs@gmail.com>
2018-02-12 08:46:36 +0100

    Create a new task list with solutions.

---
+++ practise.py
@@ -0,0 +1,17 @@
+def sumOfFactors(n):
+    sum = 0;
+    for i in range(1,int(n/2)+1):
+        if n%i==0:
+            sum+=i
+    return sum
+
+
+#--------------------------------------------------
+n = int(input('Give a number:'))
+sumOfFacts = sumOfFactors(n)
+if sumOfFacts == n:
+    print(n, 'is a prefect number')
+elif sumOfFacts < n:
+    print(n,'is a deficient number')
+else:
+    print(n,'is a abundant number')
