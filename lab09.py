9d286a67443a183b83e8fdc66d4ca4a651970f38
Balazs Harangi Ph.D <harangi.balazs@gmail.com>
2019-04-24 11:19:33 +0200

    Update lab09.py

--- lab09.py
+++ lab09.py
@@ -16,24 +16,37 @@ import keyboard
 # print(s2.issuperset(s1))
 
 # EX2:
-# wheel = set(range(1, 37))
-#
-# even = set(range(2,37,2))
-# odd = set(range(1,37,2))
-# reds = {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36}
-# blacks = wheel-reds
-# higher = set(range(19,37))
-# lower = wheel-higher
-#
-# wheel.update({'00',0})
-#
-# num = random.choice(list(wheel))
-#
-# sets = {'even':even, 'odd':odd, 'reds':reds, 'blacks':blacks, 'high':higher, 'low':lower}
-#
-# for s in sets:
-#     if num in sets[s]:
-#         print('The number {} is {}'.format(num,s))
+import random
+wheel = set(range(0,37))
+
+red = {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36}
+black = wheel-red
+even = set(range(0,37,2))
+odd = wheel-even
+small = set(range(0,19))
+great = wheel-small
+
+wheel.add('00')
+
+prop = {'red':red,'black':black,'even':even,'odd':odd,'small':small,'great':great}
+bet = input('Give your bet: ')
+while True:
+    if bet not in prop:
+        bet = input('Give your bet again: ')
+    else:
+        num = random.choice(list(wheel))
+        if num=='00':
+            print('00 is green')
+        else:
+            for k in prop:
+                if num in prop[k]:
+                    print('{} is {}'.format(num,k))
+            if num in prop[bet]:
+                print('You win!')
+                break
+            else:
+                print('You lose!')
+                bet = input('Give your bet again: ')
 
 
 # EX3:
